dbdac280b0170ef0f379e48bd74092f56479a701
elppanl <elppanl@126.com>
2018-01-30 22:39:54 +0800

    a

--- es6test/dist/index.js
+++ es6test/dist/index.js
@@ -2,6 +2,13 @@
 
 var a = '第一个ES6测试';
 document.getElementById("p1").innerText = a;
-var b = 3;
-b = 5;
+var b = 1,
+    _b1$b = { b1: 2, b2: 3 },
+    b1 = _b1$b.b1,
+    b2 = _b1$b.b2,
+    c = 5;
+
 console.log(b);
+console.log(b1);
+console.log(b2);
+console.log(c);
--- es6test/src/index.js
+++ es6test/src/index.js
@@ -1,8 +1,11 @@
 let a='第一个ES6测试';
 document.getElementById("p1").innerText=a;
-let b=3;
-b=5;
+let [b, {b1, b2}, c] = [1, {b1:2, b2:3}, 5];
 console.log(b);
+console.log(b1);
+console.log(b2);
+console.log(c);
+
 
 
 
